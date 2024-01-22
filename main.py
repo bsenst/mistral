efeacd7c120b2b8588cc1493bd0652b2e30979a5
Benjamin Senst <benjamin.senst@gmail.com>
2024-01-22 22:22:15 +0000

    update app

--- main.py
+++ main.py
@@ -1,22 +1,58 @@
 import streamlit as st
-from textblob import TextBlob
+from st_keyup import st_keyup
 
-def analyze_sentiment(text):
-    analysis = TextBlob(text)
-    polarity = analysis.sentiment.polarity
+text = st_keyup("Enter a value", key="0", value="enter a text", debounce=500, label_visibility="collapsed") + " "
 
-    if polarity > 0:
-        return "Positive"
-    elif polarity < 0:
-        return "Negative"
-    else:
-        return "Neutral"
+from deep_translator import GoogleTranslator
 
-def main():
-    st.title("Text Sentiment Analysis App")
+# Use any translator you like, in this example GoogleTranslator
+text = GoogleTranslator(source='auto', target='en').translate(text)
 
-    # Text input area
-    user_input = st.text_area("Enter text and press Enter:", key="user_input", height=100)
+st.write("Translation:", text)
 
-if __name__ == "__main__":
-    main()
+from transformers import pipeline
+
+# Load the NER pipeline with the camembert/ner-medical model
+ner_pipeline = pipeline("ner", model="d4data/biomedical-ner-all")
+
+# Example medical text
+# text = "The patient was diagnosed with diabetes and prescribed insulin."
+
+# Perform named entity recognition
+medical_entities = ner_pipeline(text)
+
+# Extract relevant information from the entities
+extracted_entities = [
+    {"text": entity["word"], "start": entity["start"], "end": entity["end"], "label": entity["entity"]}
+    for entity in medical_entities
+]
+
+medical_entities = " ".join([ent["text"] for ent in extracted_entities])
+
+# Print the extracted medical entities
+st.write("Extracted Medical Entities:", medical_entities)
+
+import clarifai
+
+from langchain.llms import Clarifai
+from langchain import PromptTemplate, LLMChain
+
+USER_ID="meta"
+APP_ID="Llama-2"
+MODEL_ID="llama2-7b-chat"
+
+llm = Clarifai(pat=st.secrets.CLARIFAI_PAT, user_id=USER_ID, app_id=APP_ID, model_id=MODEL_ID)
+
+template = """Given the following case vignette: {medical_entities}\n
+What additional questions would you ask to differentiate possible underlying diagnoses?\n
+Show the suggested questions as bullet list."""
+
+prompt = PromptTemplate(
+    template=template, 
+    input_variables=["medical_entities"]
+) 
+
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+if st.button("Submit"):
+    st.write(llm_chain.run(medical_entities))
